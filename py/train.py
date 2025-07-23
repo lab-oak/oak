battle encoding: remove multihit/flinch. fix off by one error add PKMN::durations Cont libtrain. Add way to turn compressed frames into frame vector, write frame to frame input/python. Frame add emp/nash value add libtrain.py module to totally wrap functions in python Nets: add python version of all nets, inference code is not finished, likely needs SharedBuffers again

py/train.py
@@ -1,59 +1,45 @@
-import ctypes
-import array
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
 
-import sys
-
-lib = ctypes.CDLL('./build/libtrain.so')
-
-pokemon_in_dim = 198
-active_in_dim = 212
-
-def initialize_encoded_buffers(size: int):
-    pokemon = array.array('f',)
-    pokemon.frombytes(b'\x00' * (n * pokemon_in_dim * 10 * 4))
-    active = array.array('f',)
-    active.frombytes(b'\x00' * (n * active_in_dim * 2 * 4))
-    return pokemon, active
-
-# Define function signature
-lib.read_battle_offsets.argtypes = [
-    ctypes.c_char_p,                      # path
-    ctypes.POINTER(ctypes.c_uint16),     # output buffer
-    ctypes.c_uint64                      # max_count
-]
-lib.read_battle_offsets.restype = ctypes.c_int64
-
-# Create a buffer
+import libtrain
+import net
 
 
-# Convert array to ctypes pointer
 
-# # Call the function
-# path = b"./0.battle"
-# count = 1024
-# buf = array.array('H', [0] * count)  # 'H' = uint16
-# n = lib.read_battle_offsets(path, buf_ptr, count)
+class SharedBuffers:
+    def __init__(self, batch_size):
+        self.pokemon_buffer = mp.Array('f', batch_size * 2 * 5 * net.Network.pokemon_in_dim, lock=False)
+        self.active_buffer = mp.Array('f', batch_size * 2 * 1 * net.Network.active_in_dim, lock=False)
+        self.score_buffer = mp.Array('f', batch_size * 1, lock=False)
+        self.eval_buffer = mp.Array('f', batch_size * 1, lock=False)
+        self.acc_buffer = mp.Array('f', batch_size * 512, lock = False)
 
-# # Print result
-# if n < 0:
-#     print("Error parsing file.")
-# else:
-#     print(f"Got {n} entries:", buf[:n])
+    def to_tensor(self, i = None):
+        if i is None:
+            return [
+                torch.frombuffer(self.pokemon_buffer, dtype=torch.float).view(-1, 2, 5, net.Network.pokemon_in_dim,),
+                torch.frombuffer(self.active_buffer, dtype=torch.float).view(-1, 2, 1, net.Network.active_in_dim,),
+                torch.frombuffer(self.score_buffer, dtype=torch.float).view(-1, 1),
+                torch.frombuffer(self.eval_buffer, dtype=torch.float).view(-1, 1),
+                torch.frombuffer(self.acc_buffer, dtype=torch.float).view(-1, 2, 1, 256)]
+        p, a, s, e, acc = self.to_tensor()
+        return p[i], a[i], s[i], e[i], acc[i]
 
+    def forward(self, pokemon_net, active_net, main_net, print_buffer=False):
+        p, a, s, e, acc = self.to_tensor() 
+        pokemon_out = pokemon_net.forward(p, print_buffer)
+        active_out = active_net.forward(a, print_buffer)
 
-def main():
-    if len(sys.argv) < 2:
-        print("Input: buffer-path; Program will parse all battles in buffer, using the normal encoding")
-        exit()
-    
-    buffer_path = sys.argv[1]
+        # write words to acc layer, offset by 1 for the hp entry
+        for player in range(2):
+            a_ = active_out[:, player, 0]
+            assert(a_.shape[1] == frame.ACTIVE_OUT - 1)
+            acc[:, player, 0, 1 : frame.ACTIVE_OUT] = a_
+            for _ in range(5):
+                start_index = frame.ACTIVE_OUT + _ * frame.POKEMON_OUT
+                acc[:, player, 0, (start_index + 1) : start_index + frame.POKEMON_OUT] = pokemon_out[:, player, _]
 
-    count = 1024
-    buf = array.array('H', [0] * count)  # 'H' = uint16
-    buf_ptr = ctypes.cast(ctypes.pointer(ctypes.c_uint16.from_buffer(buf)), ctypes.POINTER(ctypes.c_uint16))
-    n = lib.read_battle_offsets(bytes(buffer_path, 'utf-8'), buf_ptr, count)
-
-
-if __name__ == "__main__":
-    main()
-
+        main_net_input = torch.concat([acc[:, 0, 0, :], acc[:, 1, 0, :]], dim=1)
+        value_out, policy_out = main_net.forward(main_net_input, print_buffer)
+        return torch.sigmoid(value_out)