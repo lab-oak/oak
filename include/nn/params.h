84eeaade19587852e449f7ec8910e34455ff46fc
open-cover <admin@opencover.net>
2025-07-29 15:09:15 -0600

    reduce hidden dim size to 32 for quantized (not sure if necessary even besides the padding, but its worth a try anyway)

--- include/nn/params.h
+++ include/nn/params.h
@@ -11,8 +11,8 @@ static constexpr int active_out_dim = 55;
 static constexpr int side_out_dim =
     (1 + active_out_dim) + 5 * (1 + pokemon_out_dim);
 static constexpr int hidden_dim = 128;
-static constexpr int value_hidden_dim = 64;
-static constexpr int policy_hidden_dim = 64;
+static constexpr int value_hidden_dim = 32;
+static constexpr int policy_hidden_dim = 32;
 static constexpr int policy_out_dim =
     (static_cast<int>(Data::Species::Mew) + 1) +
     (static_cast<int>(Data::Move::Struggle) - 1); // no Struggle, None
