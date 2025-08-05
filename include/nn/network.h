51d1897b3bf2624770d5230ef4f9bf71e1068acd
open-cover <admin@opencover.net>
2025-08-04 19:34:12 -0600

    network policy inference

--- include/nn/network.h
+++ include/nn/network.h
@@ -112,6 +112,45 @@ struct Network {
     return value;
   }
 
+  float inference(const pkmn_gen1_battle &b,
+                  const pkmn_gen1_chance_durations &d, const auto m,
+                  const auto n, const auto *p1_choice_index,
+                  const auto *p2_choice_index, float *p1, float *p2) {
+    static thread_local float pokemon_input[2][5][Encode::Pokemon::n_dim];
+    static thread_local float active_input[2][1][Encode::Active::n_dim];
+    static thread_local float main_input[2][256];
+
+    const auto &battle = View::ref(b);
+    const auto &durations = View::ref(d);
+    for (auto s = 0; s < 2; ++s) {
+      const auto &side = battle.sides[s];
+      const auto &duration = durations.get(s);
+      const auto &stored = side.stored();
+
+      Encode::Active::write(stored, side.active, duration, active_input[s][0]);
+      active_net.propagate(active_input[s][0], main_input[s] + 1);
+      main_input[s][0] = (float)stored.hp / stored.stats.hp;
+
+      for (auto slot = 2; slot <= 6; ++slot) {
+        // float *input = pokemon_input[s][slot - 2];
+        float *output = main_input[s] + main_input_index(slot - 1);
+        const auto &pokemon = side.get(slot);
+
+        const auto sleep = duration.sleep(slot - 1);
+        // Encode::Pokemon::write(pokemon, sleep, input);
+        const auto *embedding =
+            pokemon_cache[s][side.order[slot - 1] - 1].get(pokemon, sleep);
+        std::memcpy(output + 1, embedding, pokemon_out_dim * sizeof(float));
+        // pokemon_net.propagate(input, output + 1);
+        output[0] = (float)pokemon.hp / pokemon.stats.hp;
+      }
+    }
+
+    float value = main_net.propagate(main_input[0], m, n, p1_choice_index,
+                                     p2_choice_index, p1, p2);
+    return value;
+  }
+
   static auto print_main_input(float *input) {
     for (auto s = 0; s < 2; ++s) {
       std::cout << "Active " << (int)(100 * input[0]) << "%\n";
