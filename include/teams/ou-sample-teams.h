75bce397ab88bc3fce10e137cc135f1f176b9b9b
open-cover <admin@opencover.net>
2025-09-22 12:25:39 -0600

    remove duplicate benchmark teams

--- include/teams/ou-sample-teams.h
+++ include/teams/ou-sample-teams.h
@@ -143,19 +143,4 @@ constexpr std::array<Team, 16> ou_sample_teams{
     },
 };
 
-constexpr std::array<Team, 2> benchmark_teams{
-    Team{Set{Species::Jynx, {Blizzard, LovelyKiss, Psychic, Rest}},
-         Set{Species::Chansey, {IceBeam, Sing, SoftBoiled, Thunderbolt}},
-         Set{Species::Cloyster, {Blizzard, Clamp, Explosion, HyperBeam}},
-         Set{Species::Rhydon, {BodySlam, Earthquake, RockSlide, Substitute}},
-         Set{Species::Starmie, {Blizzard, Recover, Thunderbolt, ThunderWave}},
-         Set{Species::Tauros, {Blizzard, BodySlam, Earthquake, HyperBeam}}},
-    Team{Set{Species::Alakazam, {Psychic, Recover, SeismicToss, ThunderWave}},
-         Set{Species::Chansey, {Reflect, SeismicToss, SoftBoiled, ThunderWave}},
-         Set{Species::Exeggutor, {Explosion, Psychic, SleepPowder, StunSpore}},
-         Set{Species::Lapras, {Blizzard, HyperBeam, Sing, Thunderbolt}},
-         Set{Species::Snorlax, {BodySlam, Earthquake, HyperBeam, SelfDestruct}},
-         Set{Species::Tauros, {Blizzard, BodySlam, Earthquake, HyperBeam}}},
-};
-
 } // namespace Teams
