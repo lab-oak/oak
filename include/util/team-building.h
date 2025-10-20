5b9abd6329bc7dc5012504f1b01b72238f1ad394
open-cover <open-cover@github.com>
2025-10-19 19:57:21 -0600

    wip teams header

--- include/util/team-building.h
+++ include/util/team-building.h
@@ -20,6 +20,61 @@ const auto team_string(const auto &team) {
   return ss.str();
 };
 
+auto softmax(auto &x) {
+  auto y = x;
+  std::transform(y.begin(), y.end(), y.begin(),
+                 [](const auto v) { return std::exp(v); });
+  const auto sum = std::accumulate(y.begin(), y.end(), 0.0);
+  std::transform(y.begin(), y.end(), y.begin(),
+                 [sum](const auto v) { return v / sum; });
+  return y;
+}
+
+auto load_teams(std::string path) {
+  std::vector<std::vector<PKMN::Set>> teams{};
+
+  if (!path.empty()) {
+
+    // read teams file
+    const auto side_to_team = [](const PKMN::Side &side) {
+      std::vector<PKMN::Set> team{};
+      for (const auto &pokemon : side.pokemon) {
+        if (pokemon.species != PKMN::Data::Species::None) {
+          PKMN::Set set{};
+          set.species = pokemon.species;
+          std::transform(pokemon.moves.begin(), pokemon.moves.end(),
+                         set.moves.begin(),
+                         [](const auto ms) { return ms.id; });
+          team.emplace_back(set);
+        }
+      }
+      return team;
+    };
+
+    std::ifstream file{path};
+    while (true) {
+      std::string line{};
+      std::getline(file, line);
+      if (line.empty()) {
+        break;
+      }
+      const auto [side, _] = Parse::parse_side(line);
+      teams.push_back(side_to_team(side));
+    }
+    if (teams.size() == 0) {
+      throw std::runtime_error{"Could not parse teams"};
+    }
+  } else {
+    // use sample teams
+    std::transform(Teams::ou_sample_teams.begin(), Teams::ou_sample_teams.end(),
+                   std::back_inserter(teams), [](const auto &team) {
+                     std::vector<PKMN::Set> t{team.begin(), team.end()};
+                     return t;
+                   });
+  }
+  return teams;
+}
+
 // Expects a full 6. If truncate, then also shuffle
 struct Omitter {
 
@@ -32,9 +87,8 @@ struct Omitter {
     if (max_pokemon < team.size()) {
       std::mt19937 rd{device.uniform_64()};
       std::shuffle(team.begin(), team.end(), rd);
+      team.resize(max_pokemon);
     }
-    team.resize(max_pokemon);
-    // std::cout << team_string(team) << std::endl;
     return (original != team);
   }
 
@@ -59,16 +113,6 @@ struct Omitter {
     return modified;
   }
 };
-
-auto softmax(auto &x) {
-  auto y = x;
-  std::transform(y.begin(), y.end(), y.begin(),
-                 [](const auto v) { return std::exp(v); });
-  const auto sum = std::accumulate(y.begin(), y.end(), 0.0);
-  std::transform(y.begin(), y.end(), y.begin(),
-                 [sum](const auto v) { return v / sum; });
-  return y;
-}
 
 [[nodiscard]] auto rollout_build_network(auto &device, auto &network,
                                          const auto &team) {
@@ -119,5 +163,76 @@ auto softmax(auto &x) {
 
   return trajectory;
 }
+
+struct Provider {
+
+  bool rb;
+  std::vector<std::vector<PKMN::Set>> teams;
+  Omitter omitter;
+  std::string network_path;
+  double modify_team_prob;
+
+  Provider(const std::string teams_path)
+      : rb{false}, teams{}, omitter{}, network{} {
+    if ((teams_path == "random-battles") || teams_path == "randbats") {
+      rb = true;
+    } else {
+      teams = load_teams(teams_path);
+    }
+  }
+
+  auto get_trajectory(auto &device)
+      -> std::pair<Train::Build::Trajectory, int> {
+
+    if (rb) {
+      Train::Build::Trajectory trajectory{};
+      const auto seed = std::bit_cast<int64_t>(device.uniform_64());
+      RandomBattles::PRNG prng{seed};
+      RandomBattles::Teams t{prng};
+      // completed but in weird format
+      const auto partial_team = t.randomTeam();
+      const auto team = t.partialToTeam(partial_team);
+      std::vector<PKMN::Set> team_vec(team.begin(), team.end());
+      trajectory.initial = team_vec;
+      trajectory.terminal = team_vec;
+      return {trajectory, -1};
+    }
+
+    const int team_index = device.random_int(teams.size());
+    auto team = teams[team_index];
+    const bool changed = omitter.shuffle_and_truncate(device, team);
+    bool deleted = false;
+    if (device.uniform() < team_modify_prob) {
+      deleted = omitter.delete_info(device, team);
+    }
+
+    if (!deleted) {
+      Train::Build::Trajectory trajectory{};
+      trajectory.initial = trajectory.terminal = team;
+      return {trajectory, team_index};
+    } else {
+      // loading each time allows the network params to be updated at runtime
+      NN::Build::Network build_network{};
+      constexpr auto tries = 3;
+      // sometimes reads fail because python is writing to that file. just retry
+      for (auto i = 0; i < tries; ++i) {
+        std::ifstream file{network_path};
+        if (build_network.read_parameters(file)) {
+          break;
+        } else {
+          if (i == (tries - 1)) {
+            throw std::runtime_error{"cant read build net params"};
+          }
+          sleep(1);
+        }
+      }
+
+      const auto trajectory =
+          TeamBuilding::rollout_build_network(device, build_network, team);
+      assert(trajectory.updates.size() > 0);
+      return {trajectory, -1};
+    }
+  }
+};
 
 } // namespace TeamBuilding
