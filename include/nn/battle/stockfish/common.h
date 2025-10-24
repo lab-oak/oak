776f3ea3c202c8f5c7c48f77c988bbda2169f8a6
open-cover <open-cover@github.com>
2025-10-23 23:21:47 -0600

    add stockfish net code

---
+++ include/nn/battle/stockfish/common.h
@@ -0,0 +1,65 @@
+/*
+  Stockfish, a UCI chess playing engine derived from Glaurung 2.1
+  Copyright (C) 2004-2025 The Stockfish developers (see AUTHORS file)
+
+  Stockfish is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  Stockfish is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+// Constants used in NNUE evaluation function
+
+#pragma once
+
+#include <algorithm>
+#include <cassert>
+#include <cstdint>
+#include <cstring>
+#include <iostream>
+#include <type_traits>
+
+// True if and only if the binary is compiled on a little-endian machine
+static inline const std::uint16_t Le = 1;
+static inline const bool IsLittleEndian =
+    *reinterpret_cast<const char *>(&Le) == 1;
+
+#include <immintrin.h>
+
+namespace NN::Battle::Stockfish {
+
+// Constant used in evaluation value calculation
+constexpr int OutputScale = 16;
+constexpr int WeightScaleBits = 6;
+
+// Size of cache line (in bytes)
+constexpr std::size_t CacheLineSize = 64;
+
+constexpr const char Leb128MagicString[] = "COMPRESSED_LEB128";
+constexpr const std::size_t Leb128MagicStringSize =
+    sizeof(Leb128MagicString) - 1;
+
+// SIMD width (in bytes)
+constexpr std::size_t SimdWidth = 32;
+
+constexpr std::size_t MaxSimdWidth = 32;
+
+// Type of input feature after conversion
+using TransformedFeatureType = std::uint8_t;
+using IndexType = std::uint32_t;
+
+// Round n up to be a multiple of base
+template <typename IntType>
+constexpr IntType ceil_to_multiple(IntType n, IntType base) {
+  return (n + base - 1) / base * base;
+}
+
+} // namespace NN::Battle::Stockfish
---
+++ include/nn/battle/stockfish/network.h
@@ -0,0 +1,87 @@
+/*
+  Stockfish, a UCI chess playing engine derived from Glaurung 2.1
+  Copyright (C) 2004-2025 The Stockfish developers (see AUTHORS file)
+
+  Stockfish is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  Stockfish is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+// Input features and network structure used in NNUE evaluation function
+
+#ifndef NNUE_ARCHITECTURE_H_INCLUDED
+#define NNUE_ARCHITECTURE_H_INCLUDED
+
+#include <cstdint>
+#include <cstring>
+#include <iosfwd>
+
+// #include "features/half_ka_v2_hm.h"
+#include "affine_transform.h"
+// #include "layers/affine_transform_sparse_input.h"
+#include "clipped_relu.h"
+// #include "layers/sqr_clipped_relu.h"
+#include "nnue_common.h"
+
+namespace NN::Battle::Stockfish {
+
+struct NetworkArchitecture {
+  static constexpr IndexType ConcatenatedSidesDims = 512;
+  static constexpr int FC_0_OUTPUTS = 32;
+  static constexpr int FC_1_OUTPUTS = 32;
+
+  Layers::AffineTransform<ConcatenatedSidesDims, FC_0_OUTPUTS> fc_0;
+  Layers::ClippedReLU<FC_0_OUTPUTS> ac_0;
+  Layers::AffineTransform<FC_0_OUTPUTS, FC_1_OUTPUTS> fc_1;
+  Layers::ClippedReLU<FC_1_OUTPUTS> ac_1;
+  Layers::AffineTransform<FC_1_OUTPUTS, 1> fc_2;
+
+  void copy_parameters(const auto &main_net) {}
+
+  std::int32_t propagate(const TransformedFeatureType *transformedFeatures) {
+    struct alignas(CacheLineSize) Buffer {
+      alignas(CacheLineSize) typename decltype(fc_0)::OutputBuffer fc_0_out;
+      alignas(CacheLineSize) typename decltype(ac_0)::OutputBuffer ac_0_out;
+      alignas(CacheLineSize) typename decltype(fc_1)::OutputBuffer fc_1_out;
+      alignas(CacheLineSize) typename decltype(ac_1)::OutputBuffer ac_1_out;
+      alignas(CacheLineSize) typename decltype(fc_2)::OutputBuffer fc_2_out;
+
+      Buffer() { std::memset(this, 0, sizeof(*this)); }
+    };
+
+#if defined(__clang__) && (__APPLE__)
+    // workaround for a bug reported with xcode 12
+    static thread_local auto tlsBuffer = std::make_unique<Buffer>();
+    // Access TLS only once, cache result.
+    Buffer &buffer = *tlsBuffer;
+#else
+    alignas(CacheLineSize) static thread_local Buffer buffer;
+#endif
+
+    fc_0.propagate(transformedFeatures, buffer.fc_0_out);
+    ac_0.propagate(buffer.fc_0_out, buffer.ac_0_out);
+    fc_1.propagate(buffer.ac_0_out, buffer.fc_1_out);
+    ac_1.propagate(buffer.fc_1_out, buffer.ac_1_out);
+    fc_2.propagate(buffer.ac_1_out, buffer.fc_2_out);
+
+    // buffer.fc_0_out[FC_0_OUTPUTS] is such that 1.0 is equal to
+    // 127*(1<<WeightScaleBits) in quantized form, but we want 1.0 to be equal
+    // to 600*OutputScale
+    std::int32_t outputValue = buffer.fc_2_out[0];
+
+    return outputValue;
+  }
+};
+
+} // namespace NN::Battle::Stockfish
+
+#endif // #ifndef NNUE_ARCHITECTURE_H_INCLUDED
