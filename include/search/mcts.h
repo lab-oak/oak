abb4d688a96edf377e61d413021bb7959068e767
open-cover <open-cover@github.com>
2025-11-16 16:27:36 -0700

    matrix ucb/mcts refactor

--- include/search/mcts.h
+++ include/search/mcts.h
@@ -26,8 +26,8 @@ struct MonteCarlo {
 
 // strategies, value estimate, etc. All info the search produces
 struct Output {
-  uint m;
-  uint n;
+  uint8_t m;
+  uint8_t n;
   size_t iterations;
   double total_value;
   double empirical_value;
@@ -45,19 +45,25 @@ struct Output {
 
 using Obs = std::array<uint8_t, 16>;
 
-struct Search {
+template <bool _root_matrix = true, bool _root_matrix_ucb = false,
+          size_t _root_matrix_ucb_delay = (1ULL << 12),
+          size_t _root_matrix_ucb_interval = (1ULL << 5),
+          size_t _root_rolls = 3, size_t _other_rolls = 1,
+          bool _debug_print = false>
+struct SearchOptions {
+  static constexpr bool root_matrix = _root_matrix;
+  static constexpr bool root_matrix_ucb = _root_matrix_ucb;
+  static constexpr size_t root_matrix_ucb_delay = _root_matrix_ucb_delay;
+  static constexpr size_t root_matrix_ucb_interval = _root_matrix_ucb_interval;
+  static constexpr size_t root_rolls = _root_rolls;
+  static constexpr size_t other_rolls = _other_rolls;
+  static constexpr bool debug_print = _debug_print;
+  // secondary
+  static constexpr bool rolls_same = (root_rolls == other_rolls);
+  static constexpr bool clamping = (root_rolls != 39) || (other_rolls != 39);
+};
 
-  template <bool _root_matrix = true, size_t _root_rolls = 3,
-            size_t _other_rolls = 1, bool _debug_print = false>
-  struct Options {
-    static constexpr bool root_matrix = _root_matrix;
-    static constexpr size_t root_rolls = _root_rolls;
-    static constexpr size_t other_rolls = _other_rolls;
-    static constexpr bool debug_print = _debug_print;
-    // secondary
-    static constexpr bool rolls_same = (root_rolls == other_rolls);
-    static constexpr bool clamping = (root_rolls != 39) || (other_rolls != 39);
-  };
+template <typename Options = SearchOptions<>> struct Search {
 
   pkmn_gen1_battle_options options;
   pkmn_gen1_chance_options chance_options;
@@ -66,21 +72,30 @@ struct Search {
   std::array<pkmn_choice, 9> p2_choices;
   std::array<std::array<uint32_t, 9>, 9> visit_matrix;
   std::array<std::array<float, 9>, 9> value_matrix;
+  // matrix ucb
+  float ucb_weight;
+  std::array<float, 9 + 2> p1_nash;
+  std::array<float, 9 + 2> p2_nash;
 
-  template <typename Options = Options<>>
   Output run(const auto dur, const auto &bandit_params, auto &node, auto &model,
              const MCTS::BattleData &input, Output output = {}) {
 
+    // reset data members
     *this = {};
 
+    // get choices data here for matrix ucb
+    output.m = pkmn_gen1_battle_choices(
+        &input.battle, PKMN_PLAYER_P1, pkmn_result_p1(input.result),
+        output.p1_choices.data(), PKMN_GEN1_MAX_CHOICES);
+    output.n = pkmn_gen1_battle_choices(
+        &input.battle, PKMN_PLAYER_P2, pkmn_result_p2(input.result),
+        output.p2_choices.data(), PKMN_GEN1_MAX_CHOICES);
+    if constexpr (Options::root_matrix_ucb) {
+      ucb_weight = std::log(2 * output.m * output.n);
+    }
+
     if (!node.is_init()) {
-      auto m = pkmn_gen1_battle_choices(
-          &input.battle, PKMN_PLAYER_P1, pkmn_result_p1(input.result),
-          p1_choices.data(), PKMN_GEN1_MAX_CHOICES);
-      auto n = pkmn_gen1_battle_choices(
-          &input.battle, PKMN_PLAYER_P2, pkmn_result_p2(input.result),
-          p2_choices.data(), PKMN_GEN1_MAX_CHOICES);
-      node.init(m, n);
+      node.init(output.m, output.n);
       if constexpr (requires {
                       node.stats().softmax_logits(nullptr, nullptr);
                       model.inference(
@@ -91,28 +106,13 @@ struct Search {
         static thread_local std::array<float, 9> p2_logits;
         const float value = model.inference(
             input.battle, *pkmn_gen1_battle_options_chance_durations(&options),
-            m, n, p1_choices.data(), p2_choices.data(), p1_logits.data(),
-            p2_logits.data());
+            output.m, output.n, output.p1_choices.data(),
+            output.p2_choices.data(), p1_logits.data(), p2_logits.data());
         node.stats().softmax_logits(p1_logits.data(), p2_logits.data());
       }
     }
 
-    // copy the state, do single mcts forward/backward, return player 1 leaf
-    // value
-    const auto prepare_and_run_iteration = [this, &bandit_params, &node, &input,
-                                            &model]() -> float {
-      auto copy = input;
-      auto *rng = reinterpret_cast<uint64_t *>(
-          copy.battle.bytes + PKMN::Layout::Offsets::Battle::rng);
-      rng[0] = model.device.uniform_64();
-      chance_options.durations = copy.durations;
-      apply_durations(copy.battle, copy.durations);
-      pkmn_gen1_battle_options_set(&options, nullptr, &chance_options, nullptr);
-      return run_iteration<Options>(bandit_params, node, copy, model).first;
-    };
-
     // Three types for 'dur' (the time investement of the search) are allowed:
-
     // chrono duration
     if constexpr (requires {
                     std::chrono::duration_cast<std::chrono::milliseconds>(dur);
@@ -122,7 +122,8 @@ struct Search {
           std::chrono::duration_cast<std::chrono::milliseconds>(dur);
       std::chrono::milliseconds elapsed{};
       while (elapsed < duration) {
-        output.total_value += prepare_and_run_iteration();
+        output.total_value +=
+            run_root_iteration(bandit_params, node, input, model, output);
         ++output.iterations;
         elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(
             std::chrono::high_resolution_clock::now() - start);
@@ -135,7 +136,8 @@ struct Search {
       const auto start = std::chrono::high_resolution_clock::now();
       while (*dur) {
         ++output.iterations;
-        output.total_value += prepare_and_run_iteration();
+        output.total_value +=
+            run_root_iteration(bandit_params, node, input, model, output);
       }
       const auto end = std::chrono::high_resolution_clock::now();
       output.duration +=
@@ -145,7 +147,8 @@ struct Search {
     } else {
       const auto start = std::chrono::high_resolution_clock::now();
       for (auto i = 0; i < dur; ++i) {
-        output.total_value += prepare_and_run_iteration();
+        output.total_value +=
+            run_root_iteration(bandit_params, node, input, model, output);
       }
       output.iterations += dur;
       const auto end = std::chrono::high_resolution_clock::now();
@@ -153,75 +156,7 @@ struct Search {
           std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
     }
 
-    // prepare output, solve empirical root matrix if enabled
-    output.empirical_value = output.total_value / output.iterations;
-    output.m = pkmn_gen1_battle_choices(
-        &input.battle, PKMN_PLAYER_P1, pkmn_result_p1(input.result),
-        output.p1_choices.data(), PKMN_GEN1_MAX_CHOICES);
-    output.n = pkmn_gen1_battle_choices(
-        &input.battle, PKMN_PLAYER_P2, pkmn_result_p2(input.result),
-        output.p2_choices.data(), PKMN_GEN1_MAX_CHOICES);
-
-    if constexpr (Options::root_matrix) {
-      // emprically shown to be reliable for 9x9 matrices, 128 bit lrslib
-      // TODO maybe use gmp since this is not the hot part of the code
-      constexpr int discretize_factor = 80;
-      std::array<int, 9 * 9> solve_matrix;
-      for (int i = 0; i < output.m; ++i) {
-        for (int j = 0; j < output.n; ++j) {
-          output.visit_matrix[i][j] += visit_matrix[i][j];
-          output.value_matrix[i][j] += value_matrix[i][j];
-          auto n = output.visit_matrix[i][j];
-          output.p1_empirical[i] += n;
-          output.p2_empirical[j] += n;
-          n += !n;
-          solve_matrix[output.n * i + j] =
-              output.value_matrix[i][j] / n * discretize_factor;
-        }
-      }
-
-      LRSNash::FastInput solve_input{static_cast<int>(output.m),
-                                     static_cast<int>(output.n),
-                                     solve_matrix.data(), discretize_factor};
-      // LRSNash convention: 2 extra entries needed for output denom, nash value
-      std::array<float, 9 + 2> nash1{}, nash2{};
-      LRSNash::FloatOneSumOutput solve_output{nash1.data(), nash2.data(), 0};
-      bool success = true;
-      try {
-        LRSNash::solve_fast(&solve_input, &solve_output);
-      } catch (std::exception &e) {
-        std::cerr << e.what() << std::endl;
-        success = false;
-        // print offending matrix in lossless form
-        std::cout << "value matrix" << std::endl;
-        for (auto i = 0; i < output.m; ++i) {
-          for (auto j = 0; j < output.n; ++j) {
-            std::cerr << "(" << output.value_matrix[i][j] << ", "
-                      << output.visit_matrix[i][j] << ") ";
-          }
-          std::cerr << std::endl;
-        }
-      }
-
-      for (int i = 0; i < output.m; ++i) {
-        output.p1_empirical[i] /= (float)output.iterations;
-        output.p1_nash[i] = nash1[i];
-      }
-      for (int j = 0; j < output.n; ++j) {
-        output.p2_empirical[j] /= (float)output.iterations;
-        output.p2_nash[j] = nash2[j];
-      }
-      output.nash_value = solve_output.value;
-
-      // set nash data to emprical rather than leave as garbage/zeros/etc
-      if (!success) {
-        output.p1_nash = output.p1_empirical;
-        output.p2_nash = output.p2_empirical;
-        output.nash_value = output.empirical_value;
-      }
-    }
-
-    return output;
+    return process_output(output);
   }
 
   // use battle seed to quickly compute a clamped damage roll
@@ -238,10 +173,137 @@ struct Search {
     }
   }
 
+  // pkmn_gen1_battle_options_set with constexpr logic
+  void battle_options_set(pkmn_gen1_battle &battle, size_t depth) {
+    if constexpr (!Options::clamping) {
+      pkmn_gen1_battle_options_set(&options, nullptr, nullptr, nullptr);
+    } else {
+      // last two bytes of battle rng
+      const auto *rand = battle.bytes + PKMN::Layout::Offsets::Battle::rng + 6;
+      auto *over = this->calc_options.overrides.bytes;
+      if constexpr (Options::rolls_same) {
+        over[0] = roll_byte<Options::root_rolls>(rand[0]);
+        over[8] = roll_byte<Options::root_rolls>(rand[1]);
+      } else {
+        if (depth == 0) {
+          over[0] = roll_byte<Options::root_rolls>(rand[0]);
+          over[8] = roll_byte<Options::root_rolls>(rand[1]);
+        } else {
+          over[0] = roll_byte<Options::other_rolls>(rand[0]);
+          over[8] = roll_byte<Options::other_rolls>(rand[1]);
+        }
+      }
+      pkmn_gen1_battle_options_set(&options, nullptr, nullptr, &calc_options);
+    }
+  }
+
+  float run_root_iteration(const auto &bandit_params, auto &node,
+                           const auto &input, auto &model, Output &output) {
+    auto copy = input;
+    auto *rng = reinterpret_cast<uint64_t *>(
+        copy.battle.bytes + PKMN::Layout::Offsets::Battle::rng);
+    rng[0] = model.device.uniform_64();
+    chance_options.durations = copy.durations;
+    apply_durations(copy.battle, copy.durations);
+    pkmn_gen1_battle_options_set(&options, nullptr, &chance_options, nullptr);
+
+    if constexpr (Options::root_matrix_ucb) {
+
+      if ((output.iterations >= Options::root_matrix_ucb_delay)) {
+
+        uint8_t p1_index{}, p2_index{};
+
+        if (output.iterations % Options::root_matrix_ucb_interval == 0) {
+
+          // get ucb matrices
+
+          std::array<int, 9 * 9> p1_ucb_matrix;
+          std::array<int, 9 * 9> p2_ucb_matrix;
+          constexpr int discretize_factor = 80;
+
+          for (auto i = 0; i < output.m; ++i) {
+            for (auto j = 0; j < output.n; ++j) {
+
+              float p1_entry = 0;
+              float p2_entry = 0;
+
+              if (visit_matrix[i][j] > 0) {
+                p1_entry = value_matrix[i][j] / visit_matrix[i][j];
+                p2_entry = 1 - p1_entry;
+              }
+
+              float exploration =
+                  std::sqrt(2 * (2 * std::log(output.iterations) + ucb_weight) /
+                            (output.iterations + 1));
+              p1_entry += exploration;
+              p2_entry += exploration;
+
+              p1_ucb_matrix[i * output.n + j] = p1_entry / discretize_factor;
+              p2_ucb_matrix[i * output.n + j] = p2_entry / discretize_factor;
+            }
+          }
+
+          // solve and sample
+          std::array<float, 9 + 2> dummy;
+          {
+            LRSNash::FastInput solve_input{
+                static_cast<int>(output.m), static_cast<int>(output.n),
+                p1_ucb_matrix.data(), discretize_factor};
+            LRSNash::FloatOneSumOutput solve_output{p1_nash.data(),
+                                                    dummy.data(), 0};
+            LRSNash::solve_fast(&solve_input, &solve_output);
+          }
+
+          {
+            LRSNash::FastInput solve_input{
+                static_cast<int>(output.m), static_cast<int>(output.n),
+                p2_ucb_matrix.data(), discretize_factor};
+            LRSNash::FloatOneSumOutput solve_output{dummy.data(),
+                                                    p2_nash.data(), 0};
+            LRSNash::solve_fast(&solve_input, &solve_output);
+          }
+        }
+
+        {
+          float p = model.device.uniform_64();
+          for (auto i = 0; i < output.m; ++i) {
+            p -= p1_nash[i];
+            if (p <= 0) {
+              break;
+            }
+          }
+        }
+
+        {
+          float p = model.device.uniform_64();
+          for (auto i = 0; i < output.n; ++i) {
+            p -= p2_nash[i];
+            if (p <= 0) {
+              break;
+            }
+          }
+        }
+
+        auto c1 = p1_choices[p1_index];
+        auto c2 = p1_choices[p2_index];
+        battle_options_set(copy.battle, 0);
+        copy.result = pkmn_gen1_battle_update(&copy.battle, c1, c2, &options);
+        const auto obs = std::bit_cast<const Obs>(
+            *pkmn_gen1_battle_options_chance_actions(&options));
+        auto &child = node(p1_index, p2_index, obs);
+
+        return run_iteration(bandit_params, child, copy, model, 1).first;
+      } else {
+        return run_iteration(bandit_params, node, copy, model).first;
+      }
+    } else {
+      return run_iteration(bandit_params, node, copy, model).first;
+    }
+  }
+
   // typical recursive mcts function
   // we return value for each player because it's slightly faster than calcing 1
   // - value at each node
-  template <typename Options>
   std::pair<float, float> run_iteration(const auto &bandit_params, auto &node,
                                         auto &input, auto &model,
                                         size_t depth = 0) {
@@ -265,32 +327,6 @@ struct Search {
       }
     };
 
-    // pkmn_gen1_battle_options_set with constexpr logic
-    const auto battle_options_set = [this, &battle, depth]() {
-      if constexpr (!Options::clamping) {
-        pkmn_gen1_battle_options_set(&options, nullptr, nullptr, nullptr);
-      } else {
-        // last two bytes of battle rng
-        const auto *rand =
-            battle.bytes + PKMN::Layout::Offsets::Battle::rng + 6;
-        auto *over = this->calc_options.overrides.bytes;
-        if constexpr (Options::rolls_same) {
-          over[0] = roll_byte<Options::root_rolls>(rand[0]);
-          over[8] = roll_byte<Options::root_rolls>(rand[1]);
-        } else {
-          if (depth == 0) {
-            over[0] = roll_byte<Options::root_rolls>(rand[0]);
-            over[8] = roll_byte<Options::root_rolls>(rand[1]);
-          } else {
-            over[0] = roll_byte<Options::other_rolls>(rand[0]);
-            over[8] = roll_byte<Options::other_rolls>(rand[1]);
-          }
-        }
-        pkmn_gen1_battle_options_set(&this->options, nullptr, nullptr,
-                                     &this->calc_options);
-      }
-    };
-
     if (node.stats().is_init()) {
       using Bandit = std::remove_reference_t<decltype(node.stats())>;
       using JointOutcome = typename Bandit::JointOutcome;
@@ -310,14 +346,14 @@ struct Search {
             PKMN::side_choice_string(battle.bytes + PKMN::Layout::Sizes::Side,
                                      c2));
 
-      battle_options_set();
+      battle_options_set(battle, depth);
       result = pkmn_gen1_battle_update(&battle, c1, c2, &options);
       const auto obs = std::bit_cast<const Obs>(
           *pkmn_gen1_battle_options_chance_actions(&options));
 
       auto &child = node(outcome.p1.index, outcome.p2.index, obs);
       const auto value =
-          run_iteration<Options>(bandit_params, child, input, model, depth + 1);
+          run_iteration(bandit_params, child, input, model, depth + 1);
       outcome.p1.value = value.first;
       outcome.p2.value = value.second;
       node.stats().update(outcome);
@@ -434,6 +470,73 @@ struct Search {
       return {.5, .5};
     }
     };
+  }
+
+  Output process_output(Output &output) {
+
+    // prepare output, solve empirical root matrix if enabled
+    output.empirical_value = output.total_value / output.iterations;
+
+    if constexpr (Options::root_matrix) {
+      // emprically shown to be reliable for 9x9 matrices, 128 bit lrslib
+      // TODO maybe use gmp since this is not the hot part of the code
+      constexpr int discretize_factor = 80;
+      std::array<int, 9 * 9> solve_matrix;
+      for (int i = 0; i < output.m; ++i) {
+        for (int j = 0; j < output.n; ++j) {
+          output.visit_matrix[i][j] += visit_matrix[i][j];
+          output.value_matrix[i][j] += value_matrix[i][j];
+          auto n = output.visit_matrix[i][j];
+          output.p1_empirical[i] += n;
+          output.p2_empirical[j] += n;
+          n += !n;
+          solve_matrix[output.n * i + j] =
+              output.value_matrix[i][j] / n * discretize_factor;
+        }
+      }
+
+      LRSNash::FastInput solve_input{static_cast<int>(output.m),
+                                     static_cast<int>(output.n),
+                                     solve_matrix.data(), discretize_factor};
+      // LRSNash convention: 2 extra entries needed for output denom, nash value
+      std::array<float, 9 + 2> nash1{}, nash2{};
+      LRSNash::FloatOneSumOutput solve_output{nash1.data(), nash2.data(), 0};
+      bool success = true;
+      try {
+        LRSNash::solve_fast(&solve_input, &solve_output);
+      } catch (std::exception &e) {
+        std::cerr << e.what() << std::endl;
+        success = false;
+        // print offending matrix in lossless form
+        std::cout << "value matrix" << std::endl;
+        for (auto i = 0; i < output.m; ++i) {
+          for (auto j = 0; j < output.n; ++j) {
+            std::cerr << "(" << output.value_matrix[i][j] << ", "
+                      << output.visit_matrix[i][j] << ") ";
+          }
+          std::cerr << std::endl;
+        }
+      }
+
+      for (int i = 0; i < output.m; ++i) {
+        output.p1_empirical[i] /= (float)output.iterations;
+        output.p1_nash[i] = nash1[i];
+      }
+      for (int j = 0; j < output.n; ++j) {
+        output.p2_empirical[j] /= (float)output.iterations;
+        output.p2_nash[j] = nash2[j];
+      }
+      output.nash_value = solve_output.value;
+
+      // set nash data to emprical rather than leave as garbage/zeros/etc
+      if (!success) {
+        output.p1_nash = output.p1_empirical;
+        output.p2_nash = output.p2_empirical;
+        output.nash_value = output.empirical_value;
+      }
+    }
+
+    return output;
   }
 };
 
