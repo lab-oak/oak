20ab1d350f4a561316a04f44d6af07d798cfefb2
open-cover <open-cover@github.com>
2025-12-14 11:31:05 -0700

    change default hyperparams

--- include/nn/default-hyperparameters.h
+++ include/nn/default-hyperparameters.h
@@ -13,8 +13,8 @@ static constexpr int active_hidden_dim = 128;
 static constexpr int active_out_dim = 83;
 static constexpr int side_out_dim =
     (1 + active_out_dim) + 5 * (1 + pokemon_out_dim);
-static constexpr int hidden_dim = 128;
-static constexpr int value_hidden_dim = 64;
+static constexpr int hidden_dim = 64;
+static constexpr int value_hidden_dim = 32;
 static constexpr int policy_hidden_dim = 64;
 static constexpr int policy_out_dim =
     static_cast<int>(PKMN::Data::Species::Mew) +
