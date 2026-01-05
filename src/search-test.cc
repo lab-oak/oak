b7ad2d5c18e4688fc983a47019a58d5aa518daa4
open-cover <open-cover@github.com>
2026-01-04 22:27:36 -0700

    search-test use argparse

--- src/search-test.cc
+++ src/search-test.cc
@@ -1,3 +1,4 @@
+#include <util/argparse.h>
 #include <util/parse.h>
 #include <util/random.h>
 #include <util/search.h>
@@ -5,11 +6,10 @@
 #include <exception>
 #include <iostream>
 
-std::string default_bandit = "exp3-0.03";
-
-RuntimeSearch::Agent default_agent{.search_time = "1_000_000",
-                                   .bandit_name = default_bandit,
-                                   .network_path = "mc"};
+struct ProgramArgs : public AgentArgs {
+  std::string &search_time =
+      kwarg("search-time", "").set_default(std::to_string(1ULL << 20));
+};
 
 constexpr float small = .03;
 
@@ -17,9 +17,8 @@ struct Test {
   std::string position;
   float expected;
   float error = small;
-  RuntimeSearch::Agent agent = default_agent;
 
-  void operator()() {
+  void operator()(const auto &args) {
     mt19937 device{std::random_device{}()};
     auto battle_data = parse_input(position, std::random_device{}());
     auto options = PKMN::options();
@@ -27,6 +26,11 @@ struct Test {
     chance_options.durations = battle_data.durations;
     pkmn_gen1_battle_options_set(&options, nullptr, &chance_options, nullptr);
     RuntimeSearch::Nodes nodes{};
+    auto agent = RuntimeSearch::Agent{.search_time = args.search_time,
+                                      .bandit_name = args.bandit_name,
+                                      .network_path = args.network_path,
+                                      .discrete_network = args.use_discrete,
+                                      .matrix_ucb_name = args.matrix_ucb_name};
     auto output = RuntimeSearch::run(device, battle_data, nodes, agent);
     bool success = std::abs(output.empirical_value - expected) <= error;
     if (!success) {
@@ -44,73 +48,79 @@ private:
   }
 };
 
-void confusion_duration() {
+void confusion_duration(const auto &args) {
   Test{
       .position = "starmie seismictoss 1hp (conf:5) | snorlax bodyslam 1hp",
       .expected = 1.0,
       .error = 0,
-  }();
+  }(args);
   Test{
       .position = "starmie seismictoss 1hp (conf:4) | snorlax bodyslam 1hp",
       .expected = .5 + .5 / 2,
-  }();
+  }(args);
   Test{
       .position = "starmie seismictoss 1hp (conf:3) | snorlax bodyslam 1hp",
       .expected = .33 + .66 / 2,
-  }();
+  }(args);
   Test{
       .position = "starmie seismictoss 1hp (conf:2) | snorlax bodyslam 1hp",
       .expected = .25 + .75 / 2,
-  }();
+  }(args);
   Test{
       .position = "starmie seismictoss 1hp (conf:1) | snorlax bodyslam 1hp",
       .expected = .5,
-  }();
+  }(args);
 }
 
-void sleep() {
+void sleep(const auto &args) {
   Test{
       .position = "starmie seismictoss 1hp slp6 | snorlax seismictoss 1hp",
       .expected = 0.0,
       .error = 0,
-  }();
+  }(args);
   Test{
       .position = "starmie seismictoss 101hp slp0 | snorlax seismictoss 1hp",
       .expected = 1.0 / 7,
-  }();
+  }(args);
   Test{
       .position = "starmie seismictoss 101hp slp1 | snorlax seismictoss 1hp",
       .expected = 1.0 / 6,
-  }();
+  }(args);
   Test{
       .position = "starmie seismictoss 101hp slp2 | snorlax seismictoss 1hp",
       .expected = 1.0 / 5,
-  }();
+  }(args);
   Test{
       .position = "starmie seismictoss 101hp slp3 | snorlax seismictoss 1hp",
       .expected = 1.0 / 4,
-  }();
+  }(args);
   Test{
       .position = "starmie seismictoss 101hp slp4 | snorlax seismictoss 1hp",
       .expected = 1.0 / 3,
-  }();
+  }(args);
   Test{
       .position = "starmie seismictoss 101hp slp5 | snorlax seismictoss 1hp",
       .expected = 1.0 / 2,
-  }();
+  }(args);
   Test{
       .position = "starmie seismictoss 101hp slp6 | snorlax seismictoss 1hp",
       .expected = 1.0,
       .error = 0,
-  }();
+  }(args);
 }
 
-void run_tests() {
-  confusion_duration();
-  sleep();
+void run_tests(const auto &args) {
+  confusion_duration(args);
+  sleep(args);
 }
 
-int main() {
-  run_tests();
+int main(int argc, char **argv) {
+
+  auto args = std::move(argparse::parse<ProgramArgs>(argc, argv));
+
+  run_tests(args);
+
+  std::cout << "All tests passed!" << std::endl;
+
   return 0;
 }
