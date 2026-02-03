83c3e53e18deff1935d7ee8c14b546e90c6e4a80
open-cover <open-cover@github.com>
2026-02-02 18:46:46 -0700

    toml, setup

---
+++ pyoak/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    include_package_data=True,
+    package_data={"pyoak": ["_native/*"]},  # include your prebuilt .so
+)
