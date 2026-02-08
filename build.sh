17c13f28fe6aac55581fde668bb2108f2c01dd72
open-cover <open-cover@github.com>
2026-02-07 17:59:12 -0700

    more

---
+++ build.sh
@@ -0,0 +1,5 @@
+chmod +x dev/libpkmn dev/release
+./dev/libpkmn
+./dev/release pyoak generate vs chall benchmark
+source .venv/bin/activate
+pip install .
--- setup.py
+++ setup.py
@@ -1,22 +1,31 @@
 from setuptools import setup, find_packages
 from setuptools import Distribution
 
+
 # Makes wheel platform-specific (because of .so)
 class BinaryDistribution(Distribution):
     def has_ext_modules(self):
         return True
 
+
 setup(
     name="oak",
-    version="1.0.2",
-    packages=find_packages("src"),  # will find 'oak' now
+    version="1.1.0",
+    packages=find_packages("src"),
     package_dir={"": "src"},
     include_package_data=True,
     package_data={
         "oak": [
-            "_native/*.so",  # include the .so files
-            "_bin/*",        # include any binaries you want packaged
+            "_native/*.so",
         ],
     },
+    data_files=[
+        ("oak/_bin", [
+            "src/oak/_bin/generate",
+            "src/oak/_bin/vs",
+            "src/oak/_bin/chall",
+            "src/oak/_bin/benchmark",
+        ]),
+    ],
     distclass=BinaryDistribution,
 )
