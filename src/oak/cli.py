98f37c27dbd74d78ad93fd6f708dcc0c66162979
open-cover <open-cover@github.com>
2026-03-09 14:00:54 -0600

    tutorial -> lab; remove elo, test-oak

--- src/oak/cli.py
+++ src/oak/cli.py
@@ -8,26 +8,17 @@ directory = "Debug" if "OAK_DEBUG" in os.environ else "Release"
 
 def _run_binary(binary_name, prefix=f"_bin/{directory}"):
     bin_path = os.path.join(os.path.dirname(__file__), prefix, binary_name)
-
-    if os.name != "nt":
-        os.chmod(bin_path, 0o755)
-
-    # Use subprocess.Popen so we can forward signals properly
+    # if os.name != "nt":
+    # os.chmod(bin_path, 0o755)
     proc = subprocess.Popen([bin_path, *sys.argv[1:]])
-
     try:
-        # Wait for the process to finish
         return_code = proc.wait()
     except KeyboardInterrupt:
-        # Forward Ctrl+C to the child process
         if os.name == "nt":
-            # On Windows, send CTRL_BREAK_EVENT (requires creationflags)
             proc.send_signal(signal.CTRL_BREAK_EVENT)
         else:
-            # On Unix, send SIGINT
             proc.send_signal(signal.SIGINT)
         return_code = proc.wait()
-
     sys.exit(return_code)
 
 
