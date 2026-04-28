c0b63d83f66137f33203224b0cda4a7dbe2e3fd7
open-cover <open-cover@github.com>
2026-04-28 16:16:26 -0600

    and missing(?) Debug binary/lib support to Oak;

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
     package_dir={"": "src"},
     include_package_data=True,
     package_data={
-        "oak": get_paths("Release"),
+        "oak": get_paths("Release") + get_paths("Debug"),
     },
     distclass=BinaryDistribution,
 )
